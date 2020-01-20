2968bca835427a5d80148bd52d1c5ddcfbd13a28
[lvliuzhen-it@bestpay.com.cn] <[ilove.mi1]>
2020-01-20 17:31:47 +0800

    提交新的方法

--- src/main/java/common/GetFileInformation.java
+++ src/main/java/common/GetFileInformation.java
@@ -1,17 +1,20 @@
 package common;
 
 import lombok.extern.slf4j.Slf4j;
+import org.testng.annotations.DataProvider;
 
 import java.io.File;
 import java.lang.reflect.Method;
-import java.util.List;
 
 
 @Slf4j
 public class GetFileInformation {
     // 获取文件路径
-    public List<String> getInformation(Method method) {
+
+    @DataProvider
+    public Object[][] getInformation(Method method) {
         // 获取 data-excel文件夹路径
+
         String filePath = GetFileInformation.class.getClassLoader().getResource("data-excel").getPath();
 
         // 获取 class类名
@@ -19,6 +22,7 @@ public class GetFileInformation {
 
         // 拼装 路径及 xlsx文件名
         String excelFilePath = filePath + File.separator + className;
+
 
 
         return null;
--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,2 +1,13 @@
-public class test {
+import common.GetFileInformation;
+import org.testng.annotations.Test;
+
+public class test extends GetFileInformation {
+
+    @Test(
+            dataProvider = "getInformation"
+    )
+
+    public void test() throws  Exception{
+        System.out.println("Hello");
+    }
 }
