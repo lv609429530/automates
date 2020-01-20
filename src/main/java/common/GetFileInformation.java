c00051e752911267039598ef730d95669dbdcd44
[lvliuzhen-it@bestpay.com.cn] <[ilove.mi1]>
2020-01-20 10:23:24 +0800

    代码新增

---
+++ src/main/java/common/GetFileInformation.java
@@ -0,0 +1,26 @@
+package common;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.lang.reflect.Method;
+import java.util.List;
+
+
+@Slf4j
+public class GetFileInformation {
+    // 获取文件路径
+    public List<String> getInformation(Method method) {
+        // 获取 data-excel文件夹路径
+        String filePath = GetFileInformation.class.getClassLoader().getResource("data-excel").getPath();
+
+        // 获取 class类名
+        String className = this.getClass().getName().substring(this.getClass().getName().lastIndexOf(".") + 1, this.getClass().getName().length());
+
+        // 拼装 路径及 xlsx文件名
+        String excelFilePath = filePath + File.separator + className;
+
+
+        return null;
+    }
+}
